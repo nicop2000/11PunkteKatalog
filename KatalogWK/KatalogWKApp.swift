49fd2c6393ffc56d4fbc17f52af73d161cf6ea99
Nico Petersen <petersen.nico@icloud.com>
2022-11-07 19:13:10 +0100

    Initial Commit

---
+++ KatalogWK/KatalogWKApp.swift
@@ -0,0 +1,20 @@
+//
+//  KatalogWKApp.swift
+//  KatalogWK
+//
+//  Created by Nico Petersen on 07.11.22.
+//
+
+import SwiftUI
+
+@main
+struct KatalogWKApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
