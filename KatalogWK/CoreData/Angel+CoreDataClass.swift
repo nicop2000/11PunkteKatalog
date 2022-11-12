f0019ed6445be4bbacfa35b2a93bf5fd6c7ee596
Nico Petersen <petersen.nico@icloud.com>
2022-11-12 12:56:13 +0100

    Rename

---
+++ KatalogWK/CoreData/Angel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Angel+CoreDataClass.swift
+//  KatalogWK
+//
+//  Created by Nico Petersen on 11.11.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Angel)
+public class Angel: NSManagedObject {
+
+}
---
+++ KatalogWK/CoreData/AngelOwner+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  AngelOwner+CoreDataClass.swift
+//  KatalogWK
+//
+//  Created by Nico Petersen on 10.11.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(AngelOwner)
+public class AngelOwner: NSManagedObject {
+
+}
